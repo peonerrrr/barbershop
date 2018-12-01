40aa8e29636d303b2e9827abb300102bd19e3111
peonerrrr <peonerrrr@gmail.com>
2018-12-01 14:57:19 +0300

    add bgcolor modal

--- jscript/script.js
+++ jscript/script.js
@@ -1,15 +1,22 @@
-var login = document.querySelector('body');
+var map = document.querySelector('.map');
+var bgcolor = document.querySelector('.bg-color');
 var closeBtn = document.querySelector('.close-btn');
 var loginForm = document.querySelector('.login-form');
 var userNavigationBtn = document.querySelector('.user-navigation-btn');
 
 
-document.querySelector('body').onclick = function(){
+document.querySelector('html').onclick = function(){
 	login.classList.add('login-form.bg');
 }
 document.querySelector('.close-btn').onclick = function(){
 	loginForm.classList.remove('show');
+	bgcolor.classList.add('visually-hidden');
 }
 document.querySelector('.user-navigation-btn').onclick = function(){
 	loginForm.classList.add('show');
+	bgcolor.classList.remove('visually-hidden');
+}
+document.querySelector('.bg-color').onclick = function(){
+	loginForm.classList.remove('show');
+	bgcolor.classList.add('visually-hidden');
 }
